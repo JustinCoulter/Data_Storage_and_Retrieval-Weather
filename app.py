5eb3ecf651b9ce95497a01c1b4c9d83b646eedc3
Justin Coulter <justincoulter@icloud.com>
2019-04-18 11:22:27 -0400

    through the last question in step 2

--- app.py
+++ app.py
@@ -51,45 +51,78 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 	"""Return a dictionary using `date` as the key and `prcp` as the value."""
-	# Query all measurements
+	
 	results = session.query(Measurement.date, Measurement.prcp).all()
 
-	# Create a dictionary from the row data and append dictionary
+	# this query actually overwrites the data and only returns one year. 
+	# but per conversation with Jason (TA), it is acceptable
+
 	measurement_dict = {}
 	for result in results:
 		date = result[0]
 		prcp = result[1]
-		measurement_dict[date] = prcp
+		measurement_dict[date] = [prcp]
 
 	return jsonify(measurement_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
+
 	""" return a JSON list all stations"""
-	results = session.query(Measurement.station).all()
+
+	results = session.query(Measurement.station).distinct().all()
 
 	return jsonify(results)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
 
-	#results =session.query(Measurement.tobs,Measurement.date).all()
+	""" returns a year's worth of temperature observations 
+	(from the last data point) for each station """
 
 	max_date = session.query(func.max(Measurement.date)).scalar()
 	year, month, day = max_date.split("-")
 	m_date = dt.date(int(year), int(month), int(day))
 	query_date = m_date - dt.timedelta(days=365)
-	data = session.query(Measurement.date,Measurement.tobs).\
-		filter(Measurement.date == query_date).all()
+	data = session.query(Measurement.date,Measurement.tobs,Measurement.station).\
+		filter(Measurement.date >= query_date).all()
 
 	return jsonify(data)
-	# sel = []
-	# results =session.query(Measurement.tobs,Measurement.date).all()
+
+@app.route("/api/v1.0/<start1>")
+def startx(start1):
+
+	startx = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start1).all()
+
+	return jsonify(startx)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def startend(start,end):
+
+	startend = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+	return jsonify(startend)
+
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
